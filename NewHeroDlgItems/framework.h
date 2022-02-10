a364dad7ea3e182e4d641fd5164a8c238e5fd74c
daemon1995 <dimonglazkov@gmail.com>
2022-02-10 23:21:14 +0300

    removed more trash

--- NewHeroDlgItems/framework.h
+++
@@ -1,6 +0,0 @@
-﻿#pragma once
-
-#define WIN32_LEAN_AND_MEAN             // Исключите редко используемые компоненты из заголовков Windows
-#define _CRT_SECURE_NO_WARNINGS
-// Файлы заголовков Windows
-#include "windows.h"
