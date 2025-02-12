6480105b7926b11b3070535bd6677e31ea716adf
daemon1995 <63409963+daemon1995@users.noreply.github.com>
2025-02-12 15:17:17 +0300

    added game numpad input support

--- GameplayEnhancementsPlugin/GameplayFeature.cpp
+++ GameplayEnhancementsPlugin/GameplayFeature.cpp
@@ -2,7 +2,10 @@
 
 namespace features
 {
-
+GameplayFeature::GameplayFeature() : IGamePatch(_PI)
+{
+    CreatePatches();
+}
 H3DlgDefButton *__stdcall H3DlgDefButton__Ctor(HiHook *h, H3DlgDefButton *bttn, int PosX, int PosY, int SizeX,
                                                int SizeY, int ItemInd, char *DefName, int cadre, int pressCadre,
                                                int CloseDialog, int HotKey, int Flags)
@@ -23,9 +26,9 @@ H3DlgDefButton *__stdcall H3DlgDefButton__Ctor(HiHook *h, H3DlgDefButton *bttn, 
     return result;
 }
 
-//void __stdcall H3DlgHero__Dismiss__BeforeRedraw(HiHook *h, int a1, int a2, int a3, int a4)
+// void __stdcall H3DlgHero__Dismiss__BeforeRedraw(HiHook *h, int a1, int a2, int a3, int a4)
 //{
-//}
+// }
 
 signed int __stdcall H3HeroDlg_Main(HiHook *h, const int heroId, int hideDelButton, int isKingdomOverView,
                                     const int isRightClick)
@@ -69,15 +72,109 @@ signed int __stdcall H3HeroDlg_Main(HiHook *h, const int heroId, int hideDelButt
     return result;
 }
 
-GameplayFeature::GameplayFeature() : IGamePatch(_PI)
+_LHF_(DlgEdit_CorrectInpulSymbol)
 {
+    // skip select scenario dialog
+    const auto dlgAddr = reinterpret_cast<DWORD>(P_WindowManager->Get()->lastDlg);
+    if (DwordAt(dlgAddr) == 0x0641CBC)
+    {
+        return EXEC_DEFAULT;
+    }
+    static UINT8 buttonsState[256];
+    // get buttons state
+    // check numlock state
+    if (GetKeyboardState(buttonsState) && buttonsState[VK_NUMLOCK])
+    {
 
-    CreatePatches();
+        if (H3Msg *msg = reinterpret_cast<H3Msg *>(c->esi))
+        {
+
+            UINT scanCode = msg->GetKey();
+
+            // if button is within range and is pressed
+            if (scanCode >= eVKey::H3VK_HOME && scanCode <= eVKey::H3VK_DELETE)
+            {
+                constexpr UINT8 VK_NPOS = 255u;
+                // assume out of range is pressed
+                UINT8 vkCode = VK_NPOS;
+                // check heroes scan code and convert it to VK
+                switch (scanCode)
+                {
+                case eVKey::H3VK_HOME:
+                    vkCode = VK_NUMPAD7;
+                    scanCode = eVKey::H3VK_7;
+
+                    break;
+                case eVKey::H3VK_UP:
+                    vkCode = VK_NUMPAD8;
+                    scanCode = eVKey::H3VK_8;
+                    break;
+                case eVKey::H3VK_PAGE_UP:
+                    vkCode = VK_NUMPAD9;
+                    scanCode = eVKey::H3VK_9;
+
+                    break;
+                case eVKey::H3VK_LEFT:
+                    vkCode = VK_NUMPAD4;
+                    scanCode = eVKey::H3VK_4;
+
+                    break;
+                case eVKey::H3VK_NUMPAD5:
+                    vkCode = VK_NUMPAD5;
+                    scanCode = eVKey::H3VK_5;
+
+                    break;
+                case eVKey::H3VK_RIGHT:
+                    vkCode = VK_NUMPAD6;
+                    scanCode = eVKey::H3VK_6;
+
+                    break;
+                case eVKey::H3VK_END:
+                    vkCode = VK_NUMPAD1;
+                    scanCode = eVKey::H3VK_1;
+
+                    break;
+                case eVKey::H3VK_DOWN:
+                    vkCode = VK_NUMPAD2;
+                    scanCode = eVKey::H3VK_2;
+
+                    break;
+                case eVKey::H3VK_PAGE_DOWN:
+                    vkCode = VK_NUMPAD3;
+                    scanCode = eVKey::H3VK_3;
+                    break;
+                case eVKey::H3VK_INSERT:
+                    vkCode = VK_NUMPAD0;
+                    scanCode = eVKey::H3VK_0;
+                    break;
+                case eVKey::H3VK_DELETE:
+                    vkCode = VK_DECIMAL;
+                    scanCode = eVKey::H3VK_PERIOD;
+                    break;
+                default:
+                    break;
+                }
+
+                if (vkCode != VK_NPOS && buttonsState[vkCode])
+                {
+                    c->eax = scanCode;
+                    msg->subtype = eMsgSubtype(scanCode);
+                }
+            }
+        }
+    }
+
+    return EXEC_DEFAULT;
 }
+
 void GameplayFeature::CreatePatches() noexcept
 {
     if (!m_isInited)
     {
+        // Adding support for NumPad keys number input @Hawaiing
+        _pi->WriteLoHook(0x05BB0B6, DlgEdit_CorrectInpulSymbol);
+
+        // Adding "SPACE" as "OK" hotkey to close any dlg
         _pi->WriteHiHook(0x455BD0, THISCALL_, H3DlgDefButton__Ctor);
 
         // Call HeroDlg creation from TownDlg with del button enabled by default
