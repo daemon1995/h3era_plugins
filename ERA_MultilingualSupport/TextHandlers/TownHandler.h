b9355c1deff68668220fe353d767fe2c32a7b14f
daemon1995 <63409963+daemon1995@users.noreply.github.com>
2025-07-29 15:47:25 +0300

    Update TownHandler.h

--- ERA_MultilingualSupport/TextHandlers/TownHandler.h
+++ ERA_MultilingualSupport/TextHandlers/TownHandler.h
@@ -6,7 +6,7 @@ class TownHandler
   public:
     struct formats
     {
-        static constexpr LPCSTR DWELLING = "era.towns.%d.dwellings.%d";
+        static constexpr LPCSTR DWELLING = "era.towns.%d.dwellings.%d.name";
         static constexpr LPCSTR DWELLING_DESCRIPTION = "era.towns.%d.dwellings.%d.description";
     };
     static constexpr int NEUTRAL_TOWN_ID = -1; // Neutral town is -1 in JSON, others are indexed
