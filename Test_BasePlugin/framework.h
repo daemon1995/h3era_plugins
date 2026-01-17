4f9dc1a1c7caf69a7ea0c66335df9a599722c75d
daemon1995 <63409963+daemon1995@users.noreply.github.com>
2026-01-17 21:08:15 +0300

    Add Test_BasePlugin project and initial source files
    Introduces the Test_BasePlugin Visual Studio project, including project configuration files, a basic DLL entry point in dllmain.cpp, and the framework.h header. This sets up the structure for a new plugin with Era platform integration and a hook initialization stub.

---
+++ Test_BasePlugin/framework.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN // Исключите редко используемые компоненты из
+                            // заголовков Windows
+// Файлы заголовков Windows
+#include <windows.h>
+// #define ERA_MODLIST
+// #define _WOG_
+#include "..\headers\header.h"
