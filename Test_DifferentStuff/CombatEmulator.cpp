c6cbf5246c3bda0c5826467757136e34d862565b
daemon1995 <63409963+daemon1995@users.noreply.github.com>
2026-04-12 20:48:30 +0300

    combat emulator  update

--- Test_DifferentStuff/CombatEmulator.cpp
+++ Test_DifferentStuff/CombatEmulator.cpp
@@ -8,33 +8,78 @@ CombatEmulator *CombatEmulator::instance = nullptr;
 //                                          seed, signed int a10, int isBank)
 
 // AdvMgr_Hero_BattleMgr_With_Neutral_Monsters(this, hero, v8, &a3, v5, PosMixed, -1, 0, 0, -1, 0, 0);
-BOOL StartRealCombat(H3Hero *attHero, H3Hero *defHero, const H3MapItem *mapItem);
-
-DllExport BOOL StartCombatEmulator(H3Hero *attHero, H3Hero *defHero, const H3MapItem *mapItem)
+BOOL StartRealCombat(H3Hero *_attHero, H3Hero *_defHero, const BOOL blockMagic, const int specialTerrain,
+                     const BOOL blockRunning);
+DllExport BOOL StartCombatEmulator(H3Hero *attHero, H3Hero *defHero, const BOOL blockMagic, const int specialTerrain,
+                                   const BOOL blockRunning)
 {
-    return StartRealCombat(attHero, defHero, mapItem);
+    return StartRealCombat(attHero, defHero, blockMagic, specialTerrain, blockRunning);
 }
-H3Army someArmy;
-H3Hero someHero;
 
-BOOL StartRealCombat(H3Hero *attHero, H3Hero *defHero, const H3MapItem *mapItem)
+struct PseudoCombatManager
 {
-    UINT pos = attHero->mixedPosition;
 
-    if (0)
+    H3Hero heroCopies[2];
+    int blockMagic;
+    int blockRunning;
+    int specialTerrain;
+
+} pseudoCmb;
+BOOL inCombat = false;
+
+enum eHeroError
+{
+    HERO_NO_ERROR,
+    HERO_ERROR_NO_HERO,
+    HERO_ERROR_WRONG_OWNER
+};
+
+eHeroError ValidateHero(H3Hero *hero)
+{
+
+    P_CombatManager->specialTerrain = 0;
+    if (!hero)
+    {
+        return HERO_ERROR_NO_HERO;
+    }
+
+    if (hero->owner < 0 || hero->owner > 7)
     {
 
-        if (mapItem)
-        {
-            pos = mapItem->GetCoordinates().Pack();
-        }
-
-        int count = 123;
-        auto result = THISCALL_12(int, 0x04AC270, P_AdventureManager->Get(), attHero, 1, &count, nullptr, pos, -1, 0, 0,
-                                  -1, 0, 0);
-
-        return 1;
+        //   return HERO_ERROR_WRONG_OWNER;
     }
+
+    return HERO_NO_ERROR;
+}
+BOOL StartRealCombat(H3Hero *_attHero, H3Hero *_defHero, const BOOL blockMagic, const int specialTerrain,
+                     const BOOL blockRunning)
+{
+
+    if (ValidateHero(_attHero) != HERO_NO_ERROR || ValidateHero(_defHero) != HERO_NO_ERROR)
+    {
+        // H3Messagebox("Invalid hero(s) provided to StartCombatEmulator. Combat will not start. Error");
+        // return 0;
+    }
+    pseudoCmb.blockMagic = blockMagic;
+    pseudoCmb.blockRunning = blockRunning;
+    pseudoCmb.specialTerrain = specialTerrain;
+
+    UINT pos = 0; // _attHero->mixedPosition;
+    // inCombat = true;
+    // if (0)
+    //{
+
+    //    if (mapItem)
+    //    {
+    //        pos = mapItem->GetCoordinates().Pack();
+    //    }
+
+    //    int count = 123;
+    //    auto result = THISCALL_12(int, 0x04AC270, P_AdventureManager->Get(), _attHero, 1, &count, nullptr, pos, -1, 0,
+    //                              0, -1, 0, 0);
+
+    //    return 1;
+    //}
 
     // P_AdventureManager->SetNextManager(P_CombatManager->Get());
     auto cmb = P_CombatManager->Get();
@@ -46,15 +91,57 @@ BOOL StartRealCombat(H3Hero *attHero, H3Hero *defHero, const H3MapItem *mapItem)
 
     //    win->SetManagers(cmb, mos);
     //	cmb->SetPreviousManager(adv);
-    adv->DemobilizeHero();
-    auto patch = _PI->WriteJmp(0x0475CF4, 0x0475D02);
- 
-	libc::memcpy(&someHero, attHero, sizeof(H3Hero));
-	someArmy = attHero->army;
-    auto result = THISCALL_11(int, 0x075ADD9, P_AdventureManager->Get(), pos, &someHero, &attHero->army, -1, nullptr,
-                              nullptr, &someArmy, -1, 1, 0);
+    // adv->DemobilizeHero();
 
-    patch->Destroy();
+    auto atkHero = &pseudoCmb.heroCopies[0];
+    auto defHero = &pseudoCmb.heroCopies[1];
+    libc::memcpy(atkHero, _attHero, sizeof(H3Hero));
+    libc::memcpy(defHero, _defHero, sizeof(H3Hero));
+    Patch *patchToBlockDlgScreen = nullptr, *patchToBlockRun = nullptr;
+    Patch *patchesToBlockAiMagic[2] = {0, 0}; // nullptr, * patchToBlockRun = nullptr;
+    if (atkHero->owner == -1)
+    {
+        patchToBlockDlgScreen = _PI->WriteJmp(0x0475CF4, 0x0475D02);
+        atkHero->owner = 1;
+    }
+    if (blockRunning)
+    {
+        patchToBlockRun = _PI->WriteJmp(0x041E6FB, 0x041EC2D);
+    }
+
+    if (defHero->owner == -1)
+    {
+        defHero->owner = 1;
+    }
+    if (blockMagic)
+    {
+
+        patchesToBlockAiMagic[0] = _PI->WriteCodePatch(0x0478191, "%n", 5);
+        patchesToBlockAiMagic[1] = _PI->WriteCodePatch(0x04781AD, "%n", 5);
+    }
+
+    //   IntAt(0x06987CC) = true;
+    auto result = THISCALL_11(int, 0x075ADD9, P_AdventureManager->Get(), pos, atkHero, &atkHero->army, 0, nullptr,
+                              defHero, &defHero->army, -1, 1, 0);
+
+    inCombat = false;
+    if (patchToBlockDlgScreen)
+    {
+        patchToBlockDlgScreen->Destroy();
+    }
+
+    if (patchToBlockRun)
+
+        patchToBlockRun->Destroy();
+
+    for (auto& patch : patchesToBlockAiMagic)
+    {
+        if (patch)
+        {
+            patch->Destroy();
+        }
+
+    }
     // CombatEmulator::GetInstance()->StartCombat(attHero, defHero, mapItem);
 
     return 1;
@@ -66,9 +153,35 @@ void CombatEmulator::Init()
     if (!instance)
     {
         instance = new CombatEmulator();
+        CreatePatches(_PI);
     }
 }
 
+void CombatEmulator::BeforeCombatStart()
+{
+}
+
+char __stdcall CombatEmulator::GameMgr_CreateSaveGameFile(HiHook *h, H3Game *gameMgr, LPCSTR saveName, const DWORD a3,
+                                                          const DWORD a4, const DWORD a5, const DWORD a6)
+{
+
+    if (inCombat)
+        return 0;
+
+    return THISCALL_6(char, h->GetDefaultFunc(), gameMgr, saveName, a3, a4, a5, a6);
+}
+void __stdcall CombatMgr_ChooseSpecialTerrain(HiHook *h, H3CombatManager *cmb)
+{
+    if (inCombat)
+    {
+        cmb->specialTerrain = pseudoCmb.specialTerrain;
+        return;
+    }
+    return THISCALL_1(void, h->GetDefaultFunc(), cmb);
+}
 void CombatEmulator::CreatePatches(PatcherInstance *_pi)
 {
+
+    _pi->WriteHiHook(0x04BEB60, THISCALL_, GameMgr_CreateSaveGameFile);
+    _pi->WriteHiHook(0x046381E, THISCALL_, CombatMgr_ChooseSpecialTerrain);
 }
--- Test_DifferentStuff/CombatEmulator.h
+++ Test_DifferentStuff/CombatEmulator.h
@@ -3,19 +3,14 @@
 class CombatEmulator
 {
 
-	static CombatEmulator* instance;
-
-
-
-
-
-
-
-
-
-
+    static CombatEmulator *instance;
 
   public:
+    void BeforeCombatStart();
+    void AfterCombatEnd();
+    static char __stdcall GameMgr_CreateSaveGameFile(HiHook *h, H3Game *gameMgr, LPCSTR saveName, const DWORD a3,
+                                                     const DWORD a4, const DWORD a5, const DWORD a6);
+
     static void CreatePatches(PatcherInstance *_pi);
-	static void Init();
+    static void Init();
 };
